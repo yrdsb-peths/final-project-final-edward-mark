949f5a5fb667e1b8363c070bcb716ac86a6769cf
edwardw878 <edwardwang878@gmail.com>
2025-05-20 14:11:27 -0400

    Added main world and main actor

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,16 @@
 import greenfoot.*;
+/**
+ * The main world
+ * 
+ * @author Mark Ku & Edward Wang
+ * @version May 2025
+ */
 
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero hero = new Hero();
+        addObject(hero, 300, 200);
     }
 }
